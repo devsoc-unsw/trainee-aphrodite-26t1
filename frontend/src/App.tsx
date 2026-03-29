55c87446023b4a45dda7a23a48dd4019ab432f75
Hanwen <hanwen@DESKTOP-KNUSI9E.localdomain>
2026-03-30 00:19:41 +1100

    feat: Added landing page via route /landing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router'
 import Home from './pages/Home'
+import LandingPage from'./pages/LandingPage.tsx'
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/landing" element={<LandingPage />}/>
       </Routes>
     </>
   )
